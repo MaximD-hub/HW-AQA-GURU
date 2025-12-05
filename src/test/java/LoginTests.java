e768029dadbcb1ff5e371962c00a55b7ebfdc34a
Max Dedyura <jwymcszcc8@privaterelay.appleid.com>
2025-12-05 09:47:28 +0500

    Update LoginTests.java
    delete email and password

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,8 +9,8 @@ public class LoginTests {
     void successfulLoginTest() {
 
         open("https://school.qa.guru/cms/system/login?required=true");
-        $x("//*[@id=\"xdgetr4926_1_1\"]/div/div[1]/input").setValue("ryolkin90@gmail.com");
-        $("[name=password]").setValue("Panisher89").pressEnter();
+        $x("//*[@id=\"xdgetr4926_1_1\"]/div/div[1]/input").setValue("");
+        $("[name=password]").setValue("").pressEnter();
         $("[class=html-content]").shouldHave(text("Вы авторизованы в этом аккаунте"));
         $(byText("Перейти в аккаунт")).click();
     }
@@ -19,7 +19,7 @@ public class LoginTests {
     void negativeLoginTest() {
 
         open("https://school.qa.guru/cms/system/login?required=true");
-        $("[name=email]").setValue("ryolkin90@gmail.com");
+        $("[name=email]").setValue("");
         $("[name=password]").setValue("qwerty").pressEnter();
         $x("//*[@id=\"xdgetr4464_1_1\"]").shouldHave(text("Неверный пароль"));
 
@@ -37,7 +37,7 @@ public class LoginTests {
     void negativeLoginTest3() {
 
         open("https://school.qa.guru/cms/system/login?required=true");
-        $x("//*[@id=\"xdgetr4926_1_1\"]/div/div[1]/input").setValue("ryolkin90@gmail.com");
+        $x("//*[@id=\"xdgetr4926_1_1\"]/div/div[1]/input").setValue("");
         $x("//*[@id=\"xdgetr4147_1_1\"]/div/div[1]/input").setValue("").pressEnter();
         $x("//*[@id=\"xdgetr4464_1_1\"]").shouldHave(text("Не заполнено поле Пароль"));
     }
